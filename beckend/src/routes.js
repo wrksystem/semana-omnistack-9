97a389e642b332373f45994cb828b4d6523a9752
Werik Rodrigues <51803873+wrksystem@users.noreply.github.com>
2021-06-14 21:45:21 -0300

    Update Server.js and Routes

---
+++ beckend/src/routes.js
@@ -0,0 +1,7 @@
+const express = require ('express');
+
+const routes = express.Router();
+
+routes.post('/users', (req, res) => {
+    return res.json(req.body);
+});
--- beckend/src/server.js
+++ beckend/src/server.js
@@ -2,8 +2,13 @@ const express = require('express');
 
 const app = express();
 
-app.post('/users', (req, res) => {
-    return res.json({ message: "Hello Omnistack"});
-});
+// GET, POST, PUT, DELETE
+
+//req.query = Acessar query params (para filtros)
+//req.params = Acessar route paramns (para edição, delete)
+//req.body = Acessar corpo da requisição (tanto para criação quanto edição de registros)
+
+app.use(express.json());
+
 
 app.listen(3333);
