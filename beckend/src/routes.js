ec00cc85d8e0838cf3148b6a788df3b4093bf4b6
Werik Rodrigues <51803873+wrksystem@users.noreply.github.com>
2021-06-14 22:34:13 -0300

    Inserção de Caminho do Banco de Dados
    Integração com o Mongo DB

--- beckend/src/routes.js
+++ beckend/src/routes.js
@@ -5,3 +5,5 @@ const routes = express.Router();
 routes.post('/users', (req, res) => {
     return res.json(req.body);
 });
+
+module.exports = routes;
--- beckend/src/server.js
+++ beckend/src/server.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const routes = require ('./routes');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://omnistack:omnistack@omnistack-9.4uprc.mongodb.net/Omnistack-9?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
 
 // GET, POST, PUT, DELETE
 
@@ -9,6 +16,7 @@ const app = express();
 //req.body = Acessar corpo da requisição (tanto para criação quanto edição de registros)
 
 app.use(express.json());
+app.use(routes);
 
 
 app.listen(3333);
